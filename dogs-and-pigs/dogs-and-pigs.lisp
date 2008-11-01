2c628270f9bc741660a72c86c71cfb1b71f84bb7
Giacomo Ritucci <giacomo.ritucci@email.it>
2008-11-01 03:42:28 +0100

    Macro with-all-accessors
    Macro per facilitare uso di with-accessors.
    
    XXX funzia solo su clisp!

--- dogs-and-pigs/dogs-and-pigs.lisp
+++ dogs-and-pigs/dogs-and-pigs.lisp
@@ -7,8 +7,20 @@
 
 (defparameter *min-hit-points* 50)
 
+
 (defmacro random-range (from to)
   `(+ ,from (random (- ,to ,from))))
+
+
+(defmacro with-all-accessors (obj cls &body body)
+  `(with-accessors
+     ,(map 'list
+	   ; da un riferimento di slot ritorna una lista (nome nome)
+	   (lambda (slot-ref)
+	     (let ((slot-name (slot-definition-name slot-ref)))
+	       (list slot-name slot-name)))
+	   (class-slots (find-class cls)))
+     ,obj ,@body))
 
 
 (defclass animal ()
@@ -39,23 +51,21 @@
 
 
 (defmethod initialize-instance :after ((this animal) &key)
-  (with-accessors ((quickness quickness)
-		   (moves moves)
-		   (strenght strenght)
-		   (defense defense)) this
-  (do ((skill-points-left *skill-points* (1- skill-points-left)))
-    ((= 0 skill-points-left))
-    (let ((skill (random 4)))
-      (cond ((= 0 skill) (incf quickness))
-	    ((= 1 skill) (incf moves))
-	    ((= 2 skill) (incf strenght))
-	    ((= 3 skill) (incf defense)))))))
+  (with-all-accessors this animal
+		      (do ((skill-points-left *skill-points* (1- skill-points-left)))
+			((= 0 skill-points-left))
+			(let ((skill (random 4)))
+			  (cond ((= 0 skill) (incf quickness))
+				((= 1 skill) (incf moves))
+				((= 2 skill) (incf strenght))
+				((= 3 skill) (incf defense)))))))
 
 
 (defmethod my-print ((this animal))
-    (format t "~a:~%" (name this))
+  (with-all-accessors this animal
+    (format t "~a:~%" name)
     (format t "~a quickness~%~a moves~%~a strenght~%~a defense~%~a hit-points~%"
-	    (quickness this) (moves this) (strenght this) (defense this) (hit-points this)))
+	    quickness moves strenght defense hit-points)))
 
 
 (defclass pig (animal)
